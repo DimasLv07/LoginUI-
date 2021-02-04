24172b4dae49fc67a3f3b9d846623f39e170a9a5
Dimas Fendriansyah <dimasfendriansyah123098@gmail.com>
2021-02-04 15:49:16 +0700

    Update

--- LoginUI;/ContentView.swift
+++ LoginUI;/ContentView.swift
@@ -6,16 +6,73 @@
 //
 
 import SwiftUI
-
+ 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        ZStack{
+            Image("co_nf")
+                .resizable()
+                .edgesIgnoringSafeArea(.all)
+            VStack{
+                HandleView()
+                FormBox()
+            }
+        }
     }
 }
-
+ 
+struct HandleView: View {
+    var body: some View{
+        VStack{
+            Image(systemName: "hand.draw.fill")
+                .resizable()
+                .frame(width: 100, height: 100)
+                .background(Color.black)
+                .foregroundColor(Color.orange)
+                .padding()
+                .background(Color.black)
+                .cornerRadius(20)
+            
+            Text("Hello Swift")
+                .foregroundColor(Color.white)
+        }
+    }
+}
+ 
+struct FormBox: View {
+    
+    @State var username: String = ""
+    @State var password: String = ""
+    
+    var body: some View{
+        VStack(alignment: .leading){
+            Text("Username").font(.callout).bold()
+            TextField("Username", text: $username)
+                .textFieldStyle(RoundedBorderTextFieldStyle())
+            
+            Text("Password").font(.callout).bold()
+            SecureField("Password", text: $password)
+                .textFieldStyle(RoundedBorderTextFieldStyle())
+            
+            Button(action: {print("Hello button")}){
+                Text("Button")
+            }
+            // ini design button
+            .padding()
+            .background(Color.black)
+            .cornerRadius(10)
+            .foregroundColor(Color.white)
+        }
+        // ini design vstack
+        .padding(.all, 20)
+        .background(Color.orange)
+        .cornerRadius(10)
+    }
+}
+ 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
     }
 }
+
